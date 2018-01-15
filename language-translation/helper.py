ed816231b0b49bf454f0c26751b26f7bf859ac38
ablecao <ablecao@tencent.com>
2018-01-15 14:23:13 +0800

    modify language-translation/helper.py 兼容python2.7

--- language-translation/helper.py
+++ language-translation/helper.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 import os
 import pickle
 import copy
@@ -12,7 +13,7 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with open(input_file, 'r') as f:
         return f.read()
 
 
